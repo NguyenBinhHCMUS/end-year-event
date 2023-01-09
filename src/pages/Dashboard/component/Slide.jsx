ea4d7b47804f76ed248822015f424010cdce11ed
ADMIN\Admin <nvtambinh@gmail.com>
2023-01-09 12:49:26 +0700

    update

--- src/pages/Dashboard/component/Slide.jsx
+++ src/pages/Dashboard/component/Slide.jsx
@@ -4,11 +4,13 @@ import macproImg from "../../../assets/img/macbook-pro-m2.png";
 import adward2 from "../../../assets/img/Picture1.png";
 import adward3 from "../../../assets/img/Picture2.png";
 import adward4 from "../../../assets/img/Picture4 (1).png";
+import adward5 from "../../../assets/img/randomImg.jpg";
 import leftIcon from "../../../assets/img/left_gh9ln8er1m6n_32 (1).png";
 import rightIcon from "../../../assets/img/right_vi24vzlqsbfj_32.png";
 import "./slide.css";
 
 const PASSWORD = "khegroup2023";
+const PASSWORD2 = "random";
 
 function Slide(props) {
   const [showModal, setShowModal] = useState(false);
@@ -17,11 +19,11 @@ function Slide(props) {
   const [adwardSelected, setAdwardSelected] = useState();
   const [password, setPassword] = useState("");
   const handlePrevSlide = () => {
-    const newSlideActive = slideActive === 0 ? 3 : slideActive - 1;
+    const newSlideActive = slideActive === 0 ? 4 : slideActive - 1;
     setSlideActive(newSlideActive);
   };
   const handleNextSlide = () => {
-    const newSlideActive = slideActive === 3 ? 0 : slideActive + 1;
+    const newSlideActive = slideActive === 4 ? 0 : slideActive + 1;
     setSlideActive(newSlideActive);
   };
 
@@ -33,7 +35,10 @@ function Slide(props) {
   };
 
   const handleComparePassword = () => {
-    if (PASSWORD === password && adwardSelected) props.onSubmit(adwardSelected);
+    if (PASSWORD === password && adwardSelected)
+      props.onSubmit(adwardSelected, 1);
+    if (PASSWORD2 === password && adwardSelected)
+      props.onSubmit(adwardSelected, 2);
   };
 
   const handleCloseModal = () => {
@@ -50,24 +55,24 @@ function Slide(props) {
   }, [slideActive, slideAuto]);
   return (
     <>
-      <div className="slide-container">
-        <div id="slide">
+      <div className='slide-container'>
+        <div id='slide'>
           <div className={`item ${slideActive === 0 && "slide-active"}`}>
-            <h2 className="slide-item__title">01 GIẢI ĐẶC BIỆT</h2>
+            <h2 className='slide-item__title'>01 GIẢI ĐẶC BIỆT</h2>
             <div
               style={{ position: "relative", width: "100%", height: "auto" }}
             >
-              <div className="image">
-                <img loading="lazy" src={macproImg} />
+              <div className='image'>
+                <img loading='lazy' src={macproImg} />
               </div>
             </div>
             <p style={{ padding: "0 12px", textAlign: "center" }}>
               Apple MacBook Air M1 256GB
             </p>
-            <div className="user-info">
+            <div className='user-info'>
               <Button
-                type="button"
-                variant="primary"
+                type='button'
+                variant='primary'
                 onClick={() => handleShow(1)}
               >
                 Tiến hành quay số
@@ -75,21 +80,21 @@ function Slide(props) {
             </div>
           </div>
           <div className={`item ${slideActive === 1 && "slide-active"}`}>
-            <h2 className="slide-item__title">01 GIẢI HẤP DẪN</h2>
+            <h2 className='slide-item__title'>01 GIẢI HẤP DẪN</h2>
             <div
               style={{ position: "relative", width: "100%", height: "auto" }}
             >
-              <div className="image">
-                <img loading="lazy" src={adward2} />
+              <div className='image'>
+                <img loading='lazy' src={adward2} />
               </div>
             </div>
             <p style={{ padding: "0 12px", textAlign: "center" }}>
               Loa Bluetooth Marshall Tufton
             </p>
-            <div className="user-info">
+            <div className='user-info'>
               <Button
-                type="button"
-                variant="primary"
+                type='button'
+                variant='primary'
                 onClick={() => handleShow(2)}
               >
                 Tiến hành quay số
@@ -97,21 +102,21 @@ function Slide(props) {
             </div>
           </div>
           <div className={`item ${slideActive === 2 && "slide-active"}`}>
-            <h2 className="slide-item__title">01 GIẢI MONG CHỜ</h2>
+            <h2 className='slide-item__title'>01 GIẢI MONG CHỜ</h2>
             <div
               style={{ position: "relative", width: "100%", height: "auto" }}
             >
-              <div className="image">
-                <img loading="lazy" src={adward3} />
+              <div className='image'>
+                <img loading='lazy' src={adward3} />
               </div>
             </div>
             <p style={{ padding: "0 12px", textAlign: "center" }}>
               Apple Watch Series 7 41mm GPS Viền Nhôm
             </p>
-            <div className="user-info">
+            <div className='user-info'>
               <Button
-                type="button"
-                variant="primary"
+                type='button'
+                variant='primary'
                 onClick={() => handleShow(3)}
               >
                 Tiến hành quay số
@@ -119,42 +124,62 @@ function Slide(props) {
             </div>
           </div>
           <div className={`item ${slideActive === 3 && "slide-active"}`}>
-            <h2 className="slide-item__title">03 GIẢI PHỤ</h2>
+            <h2 className='slide-item__title'>03 GIẢI PHỤ</h2>
             <div
               style={{ position: "relative", width: "100%", height: "auto" }}
             >
-              <div className="image">
-                <img className="image-04" loading="lazy" src={adward4} />
+              <div className='image'>
+                <img className='image-04' loading='lazy' src={adward4} />
               </div>
             </div>
             <p style={{ padding: "0 12px", textAlign: "center" }}>
               Pernod Ricard
             </p>
-            <div className="user-info">
+            <div className='user-info'>
               <Button
-                type="button"
-                variant="primary"
+                type='button'
+                variant='primary'
                 onClick={() => handleShow(4)}
               >
                 Tiến hành quay số
               </Button>
             </div>
           </div>
+          <div className={`item ${slideActive === 4 && "slide-active"}`}>
+            <h2 className='slide-item__title'>GIẢI MỞ RỘNG</h2>
+            <div
+              style={{ position: "relative", width: "100%", height: "auto" }}
+            >
+              <div className='image'>
+                <img loading='lazy' src={adward5} />
+              </div>
+            </div>
+            <p style={{ padding: "0 12px", textAlign: "center" }}></p>
+            <div className='user-info'>
+              <Button
+                type='button'
+                variant='primary'
+                onClick={() => handleShow(5)}
+              >
+                Tiến hành quay số
+              </Button>
+            </div>
+          </div>
         </div>
-        <div className="directional">
-          <button id="prev" onClick={handlePrevSlide}>
-            <img src={leftIcon} alt="" />
+        <div className='directional'>
+          <button id='prev' onClick={handlePrevSlide}>
+            <img src={leftIcon} alt='' />
           </button>
-          <button id="next" onClick={handleNextSlide}>
-            <img src={rightIcon} alt="" />
+          <button id='next' onClick={handleNextSlide}>
+            <img src={rightIcon} alt='' />
           </button>
         </div>
-        <div className="auto-slide">
+        <div className='auto-slide'>
           <Form>
             <Form.Check
-              type="switch"
-              id="custom-switch"
-              label="Tự động"
+              type='switch'
+              id='custom-switch'
+              label='Tự động'
               checked={slideAuto}
               value={slideAuto}
               onChange={() => setSlideAuto(!slideAuto)}
@@ -166,30 +191,30 @@ function Slide(props) {
         show={showModal}
         onHide={handleCloseModal}
         // fullscreen
-        size="md"
-        aria-labelledby="contained-modal-title-vcenter"
+        size='md'
+        aria-labelledby='contained-modal-title-vcenter'
         centered
       >
         <div style={{ background: "#00092f" }}>
           <Modal.Header closeButton>
             <Modal.Title>
-              <h2 className="text-center text-white w-100">
+              <h2 className='text-center text-white w-100'>
                 Nhập mật khẩu quay số
               </h2>
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <InputGroup className="my-2">
+            <InputGroup className='my-2'>
               <Form.Control
-                placeholder="Mã khẩu"
+                placeholder='Mã khẩu'
                 aria-label="Recipient's username"
-                aria-describedby="basic-addon2"
-                type="password"
+                aria-describedby='basic-addon2'
+                type='password'
                 onChange={(e) => setPassword(e.target.value)}
               />
               <Button
-                variant="primary"
-                id="button-addon2"
+                variant='primary'
+                id='button-addon2'
                 onClick={handleComparePassword}
               >
                 Kiểm tra
@@ -197,7 +222,7 @@ function Slide(props) {
             </InputGroup>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={handleCloseModal}>
+            <Button variant='secondary' onClick={handleCloseModal}>
               Đóng
             </Button>
           </Modal.Footer>
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -35,6 +35,7 @@ function Dashboard() {
   const [code, setCode] = useState();
   const [infoParticipant, setInfoParticipant] = useState();
   const [adwardSelected, setAdwardSelected] = useState();
+  const [typeRandom, setTypeRandom] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const [historyAdward, setHistoryAdward] = useState([]);
 
@@ -56,8 +57,9 @@ function Dashboard() {
     setN3(0);
   };
 
-  const handleRandomNumberSlide = (idAdward) => {
+  const handleRandomNumberSlide = (idAdward, type) => {
     setAdwardSelected(idAdward);
+    setTypeRandom(type);
     setShowRandom(true);
   };
 
@@ -75,7 +77,7 @@ function Dashboard() {
           setShowInfo(true);
           localStorage.setItem("info", JSON.stringify(data));
         } else {
-          <Alert variant="danger">
+          <Alert variant='danger'>
             Xin lỗi chúng tôi không tìm thấy thông tin của bạn!
           </Alert>;
         }
@@ -95,7 +97,7 @@ function Dashboard() {
     else setShow(true);
   };
 
-  const handleRandomNumber = async () => {
+  const handleRandomNumberType2 = async () => {
     setIsLoading(true);
     try {
       if (adwardSelected) {
@@ -111,13 +113,43 @@ function Dashboard() {
           setN2(data?.code[1]);
           setN3(data?.code[2]);
         } else {
-          <Alert variant="danger">Có lỗi vấn đề đường truyền!</Alert>;
+          <Alert variant='danger'>Có lỗi vấn đề đường truyền!</Alert>;
         }
       }
     } catch (error) {
       console.log(error);
       setIsLoading(false);
     }
+  };
+
+  const handleRandomNumber = async () => {
+    setIsLoading(true);
+    try {
+      if (adwardSelected) {
+        const response = await axios.get(
+          `https://b0ae-171-248-107-217.ap.ngrok.io/Participant/GetListRewardRecipients?rewardNo=${adwardSelected}`,
+          { withCredentials: true }
+        );
+        setIsLoading(false);
+        if (response.data?.data) {
+          const { data } = response.data;
+          setStartRandom(true);
+          setN1(data[0]?.code[0]);
+          setN2(data[0]?.code[1]);
+          setN3(data[0]?.code[2]);
+        } else {
+          <Alert variant='danger'>Có lỗi vấn đề đường truyền!</Alert>;
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  const handleRandomByType = async () => {
+    if (typeRandom === 1) await handleRandomNumber();
+    if (typeRandom === 2) await handleRandomNumberType2();
   };
 
   const handleSaveAdward = () => {
@@ -441,36 +473,36 @@ function Dashboard() {
   return (
     <>
       <header>
-        <div className="logo">
-          <img loading="lazy" src={logoImg} alt="" />
-          <img loading="lazy" src={logo2Img} alt="" />
-          <img loading="lazy" src={logo3Img} alt="" />
-          <img loading="lazy" src={logo4Img} alt="" />
+        <div className='logo'>
+          <img loading='lazy' src={logoImg} alt='' />
+          <img loading='lazy' src={logo2Img} alt='' />
+          <img loading='lazy' src={logo3Img} alt='' />
+          <img loading='lazy' src={logo4Img} alt='' />
         </div>
       </header>
       {navigator === "home" && (
-        <div className="info">
-          <div className="info-container">
-            <div className="house mt-4">
-              <img src={dateImg} alt="" />
+        <div className='info'>
+          <div className='info-container'>
+            <div className='house mt-4'>
+              <img src={dateImg} alt='' />
             </div>
             {/* <TimerCountDown /> */}
-            <div className="info-content">
-              <h1 className="info-content-title">CÁM ƠN BẠN ĐÃ ĐẾN!</h1>
+            <div className='info-content'>
+              <h1 className='info-content-title'>CÁM ƠN BẠN ĐÃ ĐẾN!</h1>
               <hr />
-              <p className="info-content__thanks">
+              <p className='info-content__thanks'>
                 Khè Group chân thành cám ơn những đóng góp to lớn của bạn cho
                 chúng tôi.
               </p>
-              <div className="info-content__address">
-                <div className="info-content__address__left">
+              <div className='info-content__address'>
+                <div className='info-content__address__left'>
                   <h3>18:00 - 22:00</h3>
                   <p>
                     Dresscode: BLACK & SILVER SHIMMER <br /> (Đen & Bạc láp
                     lánh)
                   </p>
                 </div>
-                <div className="info-content__address__right">
+                <div className='info-content__address__right'>
                   <h3>THE BAO MANSION</h3>
                   <p>755 Nguyễn Duy Trinh, P. Phú Hữu, TP Thủ Đức</p>
                 </div>
@@ -480,13 +512,13 @@ function Dashboard() {
         </div>
       )}
       {navigator === "timeline" && (
-        <div className="menu-dish-container">
+        <div className='menu-dish-container'>
           <StepperMenu />
         </div>
       )}
 
       {navigator === "menu" && (
-        <div className="menu-dish-container">
+        <div className='menu-dish-container'>
           <MenuCard />
         </div>
       )}
@@ -499,7 +531,7 @@ function Dashboard() {
         </button>
       </div> */}
 
-      <div className="backgound-image">
+      <div className='backgound-image'>
         {/* <img loading="lazy" src={backgoundImg} alt="" /> */}
       </div>
 
@@ -507,32 +539,32 @@ function Dashboard() {
         <BottomNav onSubmit={handleNavigator} />
       </footer>
 
-      <canvas id="canvas"> </canvas>
+      <canvas id='canvas'> </canvas>
       <Modal
         show={show}
         onHide={handleClose}
         // fullscreen
-        size="xl"
-        aria-labelledby="contained-modal-title-vcenter"
+        size='xl'
+        aria-labelledby='contained-modal-title-vcenter'
         centered
       >
         <div style={{ background: "#00092f" }}>
           <Modal.Header closeButton>
             <Modal.Title>
-              <h2 className="text-center text-white w-100">Xác nhận tham dự</h2>
+              <h2 className='text-center text-white w-100'>Xác nhận tham dự</h2>
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <InputGroup className="my-2">
+            <InputGroup className='my-2'>
               <Form.Control
-                placeholder="Mã dự thưởng"
+                placeholder='Mã dự thưởng'
                 aria-label="Recipient's username"
-                aria-describedby="basic-addon2"
+                aria-describedby='basic-addon2'
                 onChange={(e) => setCode(e.target.value)}
               />
               <Button
-                variant="primary"
-                id="button-addon2"
+                variant='primary'
+                id='button-addon2'
                 onClick={handleGetInfoByCode}
               >
                 Kiểm tra
@@ -540,7 +572,7 @@ function Dashboard() {
             </InputGroup>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
+            <Button variant='secondary' onClick={handleClose}>
               Đóng
             </Button>
           </Modal.Footer>
@@ -549,14 +581,14 @@ function Dashboard() {
       <Modal
         show={showInfo}
         onHide={handleCloseInfo}
-        size="xl"
-        aria-labelledby="contained-modal-title-vcenter"
+        size='xl'
+        aria-labelledby='contained-modal-title-vcenter'
         centered
       >
         <div style={{ background: "#00092f" }}>
           <Modal.Header closeButton>
             <Modal.Title>
-              <h2 className="text-center text-white w-100">
+              <h2 className='text-center text-white w-100'>
                 Thông tin khách mời
               </h2>
             </Modal.Title>
@@ -564,20 +596,20 @@ function Dashboard() {
           <Modal.Body>
             <Container>
               <Row>
-                <Col className="text-white">Họ tên: </Col>
-                <Col className="text-white">{infoParticipant?.userName}</Col>
+                <Col className='text-white'>Họ tên: </Col>
+                <Col className='text-white'>{infoParticipant?.userName}</Col>
               </Row>
               <Row>
-                <Col className="text-white">Mã số trúng thưởng: </Col>
+                <Col className='text-white'>Mã số trúng thưởng: </Col>
                 <Col style={{ color: "#f2d377" }}>{infoParticipant?.code}</Col>
               </Row>
             </Container>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={handleCloseInfo}>
+            <Button variant='secondary' onClick={handleCloseInfo}>
               Đóng
             </Button>
-            <Button variant="primary" onClick={handleCheckAgain}>
+            <Button variant='primary' onClick={handleCheckAgain}>
               Thử lại
             </Button>
           </Modal.Footer>
@@ -587,92 +619,92 @@ function Dashboard() {
         show={showRandom}
         onHide={handleCloseRandom}
         // fullscreen
-        size="xl"
-        aria-labelledby="contained-modal-title-vcenter"
+        size='xl'
+        aria-labelledby='contained-modal-title-vcenter'
         centered
       >
         <div style={{ background: "#00092f" }}>
           <Modal.Header closeButton>
             <Modal.Title>
-              <h2 className="text-center text-white w-100">
+              <h2 className='text-center text-white w-100'>
                 Mã số dự thưởng ?
               </h2>
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
             {!!isLoading ? (
-              <div className="spinner-loading">
-                <Spinner animation="border" role="status">
-                  <span className="visually-hidden">Loading...</span>
+              <div className='spinner-loading'>
+                <Spinner animation='border' role='status'>
+                  <span className='visually-hidden'>Loading...</span>
                 </Spinner>
               </div>
             ) : startRandom === false ? (
               <div className={`wrapper-number ${startRandom && "start"}`}>
-                <div className="number-area">
-                  <span className="num n1" data-attr="5741278934">
+                <div className='number-area'>
+                  <span className='num n1' data-attr='5741278934'>
                     0
                   </span>
                 </div>
-                <div className="number-area">
-                  <span className="num n2" data-attr="4785125986">
+                <div className='number-area'>
+                  <span className='num n2' data-attr='4785125986'>
                     0
                   </span>
                 </div>
-                <div className="number-area">
-                  <span className="num n3" data-attr="2478649812">
+                <div className='number-area'>
+                  <span className='num n3' data-attr='2478649812'>
                     0
                   </span>
                 </div>
               </div>
             ) : (
               <div className={`wrapper-number ${startRandom && "start"}`}>
-                <div className="number-area">
-                  <span className="num n1" data-attr="5741278934">
+                <div className='number-area'>
+                  <span className='num n1' data-attr='5741278934'>
                     {n1}
                   </span>
                 </div>
-                <div className="number-area">
-                  <span className="num n2" data-attr="4785125986">
+                <div className='number-area'>
+                  <span className='num n2' data-attr='4785125986'>
                     {n2}
                   </span>
                 </div>
-                <div className="number-area">
-                  <span className="num n3" data-attr="2478649812">
+                <div className='number-area'>
+                  <span className='num n3' data-attr='2478649812'>
                     {n3}
                   </span>
                 </div>
               </div>
             )}
 
-            <p className="text-center text-white">
+            <p className='text-center text-white'>
               {historyAdward
                 .filter((item) => item?.adward === adwardSelected)
                 .map((item) => (
-                  <span className="p-2">{item?.code}</span>
+                  <span className='p-2'>{item?.code}</span>
                 ))}
             </p>
 
-            <div className="w-100 d-flex align-items-center justify-content-center">
+            <div className='w-100 d-flex align-items-center justify-content-center'>
               <Button
-                variant="primary"
-                className="mx-2"
+                variant='primary'
+                className='mx-2'
                 style={{ width: "100px" }}
-                onClick={handleRandomNumber}
+                onClick={handleRandomByType}
               >
                 Quay
               </Button>
               <Button
-                variant="success"
-                className="mx-2"
+                variant='success'
+                className='mx-2'
                 style={{ width: "100px" }}
                 onClick={() => handleResetNumber()}
               >
                 Làm mới
               </Button>
               <Button
-                className="mx-2"
+                className='mx-2'
                 style={{ width: "100px" }}
-                variant="primary"
+                variant='primary'
                 onClick={handleSaveAdward}
               >
                 Lưu
@@ -680,7 +712,7 @@ function Dashboard() {
             </div>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={handleCloseRandom}>
+            <Button variant='secondary' onClick={handleCloseRandom}>
               Đóng
             </Button>
           </Modal.Footer>
